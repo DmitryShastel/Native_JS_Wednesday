ce5fb083bc4494d69dd86086fca2944b5ba600dc
DmitryShastel <dmshastel@gmail.com>
2021-04-12 18:54:09 +0300

    n_js_lesson2 tasks 1-13-done

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -137,3 +137,81 @@ export default () => {};
 
 //sum(1)(2) = 3
 //sum(5)(-1) = 4
+
+
+//Task 11
+//У нас есть встроенный метод arr.filter(f) для массивов. Он фильтрует все элементы с помощью функции f.
+// Если она возвращает true, то элемент добавится в возвращаемый массив.
+//Сделайте набор «готовых к употреблению» фильтров:
+//inBetween(a, b) – между a и b (включительно).
+//inArray([...]) – находится в данном массиве.
+//Они должны использоваться таким образом:
+//arr.filter(inBetween(3,6)) – выбирает только значения между 3 и 6 (включительно).
+//arr.filter(inArray([1,2,3])) – выбирает только элементы, совпадающие с одним из элементов массива
+
+//function inBetween(a, b) {
+//    return function(x) {
+//        return x >= a && x <= b;
+//    }
+//}
+//let arr = [1, 2, 3, 4, 5, 6, 7];
+//alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+//function inArray(arr) {
+//    return function(x) {
+//        return arr.includes(x);
+//   };
+//}
+//let arr = [1, 2, 3, 4, 5, 6, 7];
+//alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+
+//Task 12
+//У нас есть массив объектов, который нужно отсортировать:
+//let users = [
+//    { name: "John", age: 20, surname: "Johnson" },
+//    { name: "Pete", age: 18, surname: "Peterson" },
+//    { name: "Ann", age: 19, surname: "Hathaway" }
+//];
+
+//function userSortName (arr){
+//    return users.sort((a, b) => a.name > b.name ? 1 : -1)
+//}
+
+//function userSortAge (arr){
+//    return users.sort((a, b) => a.age > b.age ? 1 : -1)
+//}
+
+//Task 13
+//Следующий код создаёт массив из стрелков (shooters).
+//Каждая функция предназначена выводить их порядковые номера. Но что-то пошло не так…
+
+//function makeArmy() {
+//    let shooters = [];
+
+//    let i = 0;
+//    while (i < 10) {
+//        let shooter = function() { // функция shooter
+//            alert( i ); // должна выводить порядковый номер
+//        };
+//        shooters.push(shooter);
+//        i++;
+//    }
+//
+//    return shooters;
+//}
+
+//let army = makeArmy();
+
+//army[0](); // у 0-го стрелка будет номер 10
+//army[5](); // и у 5-го стрелка тоже будет номер 10
+// ... у всех стрелков будет номер 10, вместо 0, 1, 2, 3...//
+
+
+
+
+
+
+
+
+
