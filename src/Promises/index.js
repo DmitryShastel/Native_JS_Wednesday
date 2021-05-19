6bcfce33447217143c5fc2234a7c5bd0b23ebd2b
DmitryShastel <dmshastel@gmail.com>
2021-05-19 19:34:16 +0300

    Promise_then

--- src/Promises/index.js
+++ src/Promises/index.js
@@ -1,7 +1,15 @@
 //import axios from "/axios";
 
+const axios = {}
 const findUserInDB = (id) => {}
 
-//const promise1 = axios.get('https://google.com')
+const promise1 = axios.get('https://google.com')
+promise1.then((data) => {
+    console.log(data)
+})
+
 const promise2 = findUserInDB(1)
+promise2.then( (user) => {
+    console.log(user)
+})
 console.log('finish')
