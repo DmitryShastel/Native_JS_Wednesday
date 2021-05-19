a7214a6003a2c334bd99e24adbf7d11848456b3b
DmitryShastel <dmshastel@gmail.com>
2021-05-19 22:25:32 +0300

    Promise_Promise.allSettled

--- src/Promises/index.js
+++ src/Promises/index.js
@@ -1,6 +1,6 @@
 //import axios from "/axios";
 
-const axios = {}
+/*const axios = {}
 const findUserInDB = (id) => {}
 
 const promise1 = axios.get('https://google.com')
@@ -12,16 +12,39 @@ const promise2 = findUserInDB(100)
 promise2
     .then((user) => {
         console.log(user)
-    })
+    })*/
 
 
-const otherPromise = Promise.all([promise1, promise2])
-
+//const otherPromise = Promise.all([promise1, promise2])
+/*
 otherPromise
-    .then( () => {
-        console.log('finish')
+    .then( (results) => {
+        const dataFromGogle = results[0]
+        const userFromDB = results[1]
+        console.log(dataFromGogle.data.vacancies + '; ' +  userFromDB.name)
     })
     .catch( () => {
         console.log('initialization failed. Try later')
     })
+*/
 
+/*const otherPromise2 = Promise.allSettled([promise1, promise2])
+
+otherPromise2
+    .then((results) => {
+        console.log(results)
+
+        const dataFromGogle =
+            results[0].status === 'fulfilled'
+                ? results[0].value
+                : {data: {vacansies: null}}
+
+        const userFromDB = results[1].status === 'fulfilled'
+            ? results[1].value
+            : {name: results[1].reson}
+        console.log(dataFromGogle.data.vacansies + '; ' + userFromDB.name)
+    })*/
+
+
+const resolvedPromise = Promise.resolve(100)
+console.log(resolvedPromise)
