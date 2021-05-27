1564aaabf9a961849f8297cbac603307ce30a5e7
DmitryShastel <dmshastel@gmail.com>
2021-05-27 21:58:38 +0300

    Promise_function - axios

--- src/Promises/index.js
+++ src/Promises/index.js
@@ -1,5 +1,44 @@
 //import axios from "/axios";
 
+var axios = {
+    _fake(url, data) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                let responseData = {
+                    text: `${url} loves you`
+                };
+                if (url.indexOf('it-kamasutra') > 0) {
+                    responseData = {
+                        requestedCount: data.count,
+                        message: 'we will prepare students for you'
+                    }
+                }else if (url.indexOf('google') > 0) {
+                    responseData = {
+                        vacancies: 12
+                    }
+                } else if (url.indexOf('microsoft') > 0) {
+                    responseData = {
+                        vacancies: 21
+                    }
+                }
+                resolve({
+                    request: {},
+                    status: 200,
+                    headers: {},
+                    config: {},
+                    data: responseData
+                })
+            }, randomInFromInterval(1, 5) * 1000);
+        })
+    },
+
+    post(url, data) {
+        return this._fake(url, data);
+    },
+    get(url, data) {
+        return this._fake(url, data);
+    }
+}
 var findUserInDB = (id) => {
     const users = [
         { id: 1, name: 'Dimych', friend: 3},
@@ -18,7 +57,6 @@ var findUserInDB = (id) => {
         }, randomInFromInterval(500, 1500))
     })
 }
-
 function randomInFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
