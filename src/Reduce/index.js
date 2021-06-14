3b6d314bf16f94ae8cced0e2fc0673d1e2ac3ac4
DmitryShastel <dmshastel@gmail.com>
2021-06-14 18:52:16 +0300

    n_js_Reduce_task№3

--- src/Reduce/index.js
+++ src/Reduce/index.js
@@ -14,7 +14,7 @@ console.log(b)*/
 // Дан массив с числами. Найдите сумму первых N элементов до первого нуля.
 // Пример: [1, 2, 3,  0, 4, 5,6] - суммируем первые 3 элемента, так как дальше стоит элемент с числом 0
 
-let arr = [1, 2, 3, 0, 4, 5, 6];
+/*let arr = [1, 2, 3, 0, 4, 5, 6];
 let answer = 0;
 let result = arr.reduce(function(sum, elem) {
     if (elem == 0) {
@@ -24,10 +24,23 @@ let result = arr.reduce(function(sum, elem) {
     }
 });
 if(answer == undefined){answer = result};
+alert(answer);*/
+
+//Дан массив с числами. Найдите сумму последних N элементов до первого нуля с конца.
+// Пример: [1, 2, 3, 0, 4, 5, 6] - суммируем последние 3 элемента, так как дальше стоит элемент с числом 0
+
+
+let arr = [1, 2, 3, 0, 4, 5, 6];
+let answer = 0;
+let result = arr.reduceRight(function(sum, elem) {
+    if (elem == 0) {
+        answer = sum;
+    } else {
+        return sum + elem;
+    }
+});
+if(answer == undefined){answer = result};
 alert(answer);
-
-
-
 
 
 
