a8032384ee3f63151b87d99014a962d564abcd8b
DmitryShastel <dmshastel@gmail.com>
2021-06-14 18:40:57 +0300

    n_js_Reduce_task№2

--- src/Reduce/index.js
+++ src/Reduce/index.js
@@ -1,10 +1,35 @@
 // Дан массив с числами. Найдите сумму этих чисел.
 
+/*
 let a = [11, 3, 4, 88]
 
 let b = a.reduce((acc, item) => {
     return acc + item
 
-},10)
+},0)
 
-console.log(b)
+console.log(b)*/
+
+
+// Дан массив с числами. Найдите сумму первых N элементов до первого нуля.
+// Пример: [1, 2, 3,  0, 4, 5,6] - суммируем первые 3 элемента, так как дальше стоит элемент с числом 0
+
+let arr = [1, 2, 3, 0, 4, 5, 6];
+let answer = 0;
+let result = arr.reduce(function(sum, elem) {
+    if (elem == 0) {
+        answer = sum;
+    } else {
+        return sum + elem;
+    }
+});
+if(answer == undefined){answer = result};
+alert(answer);
+
+
+
+
+
+
+
+
