49091e25034f4ee44f89d353963027934de2ca46
DmitryShastel <dmshastel@gmail.com>
2021-06-14 19:01:54 +0300

    n_js_Reduce_task№4

--- src/Reduce/index.js
+++ src/Reduce/index.js
@@ -29,8 +29,7 @@ alert(answer);*/
 //Дан массив с числами. Найдите сумму последних N элементов до первого нуля с конца.
 // Пример: [1, 2, 3, 0, 4, 5, 6] - суммируем последние 3 элемента, так как дальше стоит элемент с числом 0
 
-
-let arr = [1, 2, 3, 0, 4, 5, 6];
+/*let arr = [1, 2, 3, 0, 4, 5, 6];
 let answer = 0;
 let result = arr.reduceRight(function(sum, elem) {
     if (elem == 0) {
@@ -40,7 +39,32 @@ let result = arr.reduceRight(function(sum, elem) {
     }
 });
 if(answer == undefined){answer = result};
-alert(answer);
+alert(answer);*/
+
+//Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось
+// больше 10-ти
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let num = 0;
+
+let result = arr.reduce((acc, item) => {
+    if(acc > 10 ){
+        console.log(num);
+        return;
+    }else {
+        num++;
+        return acc + item;
+    }
+})
+
+
+
+
+
+
+
+
+
 
 
 
